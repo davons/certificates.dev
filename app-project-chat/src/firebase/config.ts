2fef5690242ad5619ad4d8740924968f1940e56a
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-06-23 11:12:54 +0200

    .env

--- app-project-chat/src/firebase/config.ts
+++ app-project-chat/src/firebase/config.ts
@@ -3,16 +3,21 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvzC9kx6C_ew8fPRVdc-d3Q4XZ3qiCy7g",
-  authDomain: "app-project-chat-c2cb3.firebaseapp.com",
-  projectId: "app-project-chat-c2cb3",
-  storageBucket: "app-project-chat-c2cb3.firebasestorage.app",
-  messagingSenderId: "9388616620",
-  appId: "1:9388616620:web:8215ebc258c6b943076809"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firebase services
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth }
+// Export services
+export { db, auth };
