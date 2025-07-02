67351391d1ecfe5c24701aa25a2ddb7c0cee7e71
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-07-02 15:56:37 +0200

    .env.example

---
+++ app-project-masterclass/src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 2 } }">Projects 2 </RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
